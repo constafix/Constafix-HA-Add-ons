95bfc9a84839f50217197835a647b404289d9e79
constafix <dk2513086@yandex.ru>
2025-05-22 20:59:28 +0300

    kakamit4

--- PostgreSQL-server/run.sh
+++ PostgreSQL-server/run.sh
@@ -1,33 +1,31 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bashio
 
-# Чтение настроек из config.yaml, который HA превращает в /data/options.json
-DB_NAME=$(jq --raw-output '.database' /data/options.json)
-DB_USER=$(jq --raw-output '.username' /data/options.json)
-DB_PASS=$(jq --raw-output '.password' /data/options.json)
+DB_NAME=$(bashio::config 'database')
+DB_USER=$(bashio::config 'username')
+DB_PASS=$(bashio::config 'password')
 
 export PGDATA="/var/lib/postgresql/data"
 
-# Инициализация, если база ещё не инициализирована
+# Инициализация БД, если не существует
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-  echo "[INFO] Инициализация PostgreSQL в $PGDATA"
-  initdb --username=postgres
+    echo "[INFO] Инициализация PostgreSQL в $PGDATA"
+    su-exec postgres initdb
 fi
 
 # Запуск PostgreSQL в фоне
 echo "[INFO] Запуск PostgreSQL..."
 su-exec postgres postgres &
 
-# Ждём, пока сервер запустится
+# Ожидание запуска
 sleep 5
 
-# Создание пользователя и базы, если они ещё не созданы
-psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
-  psql -U postgres -c "CREATE DATABASE \"$DB_NAME\""
+# Создание БД и пользователя
+su-exec postgres psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
+    su-exec postgres psql -U postgres -c "CREATE DATABASE \"$DB_NAME\""
 
-psql -U postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = '$DB_USER'" | grep -q 1 || \
-  psql -U postgres -c "CREATE USER \"$DB_USER\" WITH ENCRYPTED PASSWORD '$DB_PASS'"
+su-exec postgres psql -U postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = '$DB_USER'" | grep -q 1 || \
+    su-exec postgres psql -U postgres -c "CREATE USER \"$DB_USER\" WITH ENCRYPTED PASSWORD '$DB_PASS'"
 
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$DB_NAME\" TO \"$DB_USER\""
+su-exec postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$DB_NAME\" TO \"$DB_USER\""
 
-# Ожидание завершения
 wait

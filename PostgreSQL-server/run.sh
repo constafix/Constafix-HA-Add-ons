92f89b6f96ab463b20abe7c8a6d2a267db711b08
constafix <dk2513086@yandex.ru>
2025-05-22 20:51:13 +0300

    kakamit0

--- PostgreSQL-server/run.sh
+++ PostgreSQL-server/run.sh
@@ -6,18 +6,9 @@ DB_NAME=$(jq --raw-output '.database' $CONFIG_PATH)
 DB_USER=$(jq --raw-output '.username' $CONFIG_PATH)
 DB_PASS=$(jq --raw-output '.password' $CONFIG_PATH)
 
-export PGDATA="/data/postgresql"
+export PGDATA="/var/lib/postgresql/data"
 
-# Создание каталога и установка прав
-mkdir -p "$PGDATA"
-chown -R postgres:postgres "$PGDATA"
+# Инициализация — PostgreSQL делает это автоматически, если папка пуста
 
-# Инициализация базы данных, если необходимо
-if [ ! -d "$PGDATA/base" ]; then
-    echo "[INFO] Инициализация PostgreSQL в $PGDATA"
-    su-exec postgres:postgres initdb -D "$PGDATA"
-fi
-
-# Запуск PostgreSQL
 echo "[INFO] Запуск PostgreSQL..."
-exec su-exec postgres:postgres postgres -D "$PGDATA"
+exec su-exec postgres postgres

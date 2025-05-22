6b14ebc1abef1436ff65dc43b78c0ac9d0b4f536
constafix <dk2513086@yandex.ru>
2025-05-22 20:15:08 +0300

    zero-test

--- PostgreSQL-server/run.sh
+++ PostgreSQL-server/run.sh
@@ -1,13 +1,3 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 
-DATABASE=$(bashio::config 'database')
-USERNAME=$(bashio::config 'username')
-PASSWORD=$(bashio::config 'password')
-PGDATA=$(bashio::config 'pgdata')
-
-export POSTGRES_DB=$DATABASE
-export POSTGRES_USER=$USERNAME
-export POSTGRES_PASSWORD=$PASSWORD
-export PGDATA=$PGDATA
-
-exec docker-entrypoint.sh postgres
+echo "Hello world!"

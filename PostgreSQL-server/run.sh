cff20e0cfc66163d7d14fad1ed1c5e98f3a28038
constafix <dk2513086@yandex.ru>
2025-05-22 21:16:28 +0300

    kakakakakakkakkakakakakakaka

--- PostgreSQL-server/run.sh
+++ PostgreSQL-server/run.sh
@@ -1,31 +1,42 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-DB_NAME=$(bashio::config 'database')
-DB_USER=$(bashio::config 'username')
-DB_PASS=$(bashio::config 'password')
+# Путь к файлу с опциями, смонтированному HA
+CONFIG=/data/options.json
+
+DB_NAME=$(jq --raw-output '.database' $CONFIG)
+DB_USER=$(jq --raw-output '.username' $CONFIG)
+DB_PASS=$(jq --raw-output '.password' $CONFIG)
 
 export PGDATA="/var/lib/postgresql/data"
 
-# Инициализация БД, если не существует
+# Инициализация кластера, если ещё не инициализирован
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-    echo "[INFO] Инициализация PostgreSQL в $PGDATA"
-    su-exec postgres initdb
+  echo "[INFO] Инициализация PostgreSQL в $PGDATA"
+  su-exec postgres initdb
 fi
 
-# Запуск PostgreSQL в фоне
+# Запуск сервера
 echo "[INFO] Запуск PostgreSQL..."
 su-exec postgres postgres &
 
-# Ожидание запуска
+# Даем время на старт
 sleep 5
 
-# Создание БД и пользователя
-su-exec postgres psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || \
-    su-exec postgres psql -U postgres -c "CREATE DATABASE \"$DB_NAME\""
+# Создаем базу, если её нет
+if ! su-exec postgres psql -U postgres -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
+  echo "[INFO] Создание базы $DB_NAME"
+  su-exec postgres psql -U postgres -c "CREATE DATABASE \"$DB_NAME\""
+fi
 
-su-exec postgres psql -U postgres -tc "SELECT 1 FROM pg_roles WHERE rolname = '$DB_USER'" | grep -q 1 || \
-    su-exec postgres psql -U postgres -c "CREATE USER \"$DB_USER\" WITH ENCRYPTED PASSWORD '$DB_PASS'"
+# Создаем пользователя, если его нет
+if ! su-exec postgres psql -U postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$DB_USER'" | grep -q 1; then
+  echo "[INFO] Создание пользователя $DB_USER"
+  su-exec postgres psql -U postgres -c "CREATE USER \"$DB_USER\" WITH ENCRYPTED PASSWORD '$DB_PASS'"
+fi
 
+# Выдаем права
+echo "[INFO] Выдаем привилегии на базу $DB_NAME пользователю $DB_USER"
 su-exec postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE \"$DB_NAME\" TO \"$DB_USER\""
 
+# Ожидание дочерних процессов
 wait
